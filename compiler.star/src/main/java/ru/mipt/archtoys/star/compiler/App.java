996a751edafa06fe99234c842cc67d9998047e46
Necto <necto.ne@gmail.com>
2012-03-20 23:38:26 +0400

    initial commit

---
+++ compiler.star/src/main/java/ru/mipt/archtoys/star/compiler/App.java
@@ -0,0 +1,13 @@
+package ru.mipt.archtoys.star.compiler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
